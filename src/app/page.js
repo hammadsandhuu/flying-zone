5eb7277df3f658fb277f1a2080ae1728da252912
hammadsandhuu <hammadsandhuu@gmail.com>
2026-03-28 21:22:33 +0500

    refactor: consolidate Hajj and Umrah navigation and replace Hajj packages page with new Hajj & Umrah landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,13 @@
-import AboutSection from "@/components/about/AboutSection";
 import Home1Activities from "@/components/activities/Home1Activities";
-import HomeSection from "@/components/banner/HomeSection";
-import BookingForm from "@/components/banner/BookingForm";
 import Home1Banner2 from "@/components/banner/Home1Banner2";
-import Home1Blog from "@/components/blog/Home1Blog";
-import DestinationSection from "@/components/destination/DestinationSection";
-import Home1FacilitySlide from "@/components/facilitySlide/Home1FacilitySlide";
-import FacilitySection from "@/components/facilitySlide/FacilitySection";
-import Home1Testimonail from "@/components/testimonial/Home1Testimonail";
-import VisaSection from "@/components/visaComponents/VisaSection";
-import Home1WhyChoose from "@/components/whyChoose/Home1WhyChoose";
-import Home3About from "@/components/about/Home3About";
-import Home3Banner3 from "@/components/banner/Home3Banner3";
 import Home3Features from "@/components/feature/Home3Features";
 import Home4Vissa from "@/components/visaComponents/Home4Vissa";
 import Home4Testimonail from "@/components/testimonial/Home4Testimonail";
-import Home4Banner2 from "@/components/banner/Home4Banner2";
-import Home5WhyChoose from "@/components/whyChoose/Home5WhyChoose";
 import Home6About from "@/components/about/Home6About";
 import Home6Feature from "@/components/feature/Home6Feature";
 import Home2Destinationslide from "@/components/destinationSlider/Home2Destinationslide";
-import Home2VideoSection from "@/components/videoSection/Home2VideoSection";
-import Home2Team from "@/components/team/Home2Team";
 import Home3DestinationSlide from "@/components/destinationSlider/Home3DestinationSlide";
 import Home3Team from "@/components/team/Home3Team";
-import Home3Blog from "@/components/blog/Home3Blog";
-import Home2Banner from "@/components/banner/Home2Banner";
 import Home3Banner from "@/components/banner/Home3Banner";
 import Home3Destination from "@/components/destination/Home3Destination";
 
@@ -41,8 +23,6 @@ export default function Home() {
   return (
     <>
       <Home3Banner />
-      {/* <Home2Banner /> */}
-      {/* <HomeSection /> */}
       <Home6About />
       <Home2Destinationslide />
       <Home4Vissa />
@@ -54,6 +34,8 @@ export default function Home() {
       <Home4Testimonail />
       <Home1Banner2 />
       <Home3Team />
+      {/* <Home2Banner /> */}
+      {/* <HomeSection /> */}
       {/* <Home3Blog /> */}
       {/* <Home3Banner3 /> */}
       {/* <DestinationSection /> */}
@@ -68,7 +50,6 @@ export default function Home() {
       {/* <Home1Banner2 /> */}
       {/* <Home3Features /> */}
       {/* <Home4Vissa /> */}
-
       {/* <Home3Banner3 /> */}
       {/* <Home1Blog /> */}
       {/* <Home2Team /> */}
